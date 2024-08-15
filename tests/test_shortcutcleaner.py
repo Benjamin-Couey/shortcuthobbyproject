13e96877a322348a479b72e1832fa5032cb9b2d3
Benjamin-Couey <benjamincouey36@gmail.com>
2024-08-15 09:33:25 -0600

    Added tests for is_broken_shortcut

--- tests/test_shortcutcleaner.py
+++ tests/test_shortcutcleaner.py
@@ -1,4 +1,6 @@
+import pytest
 from shortcutcleaner.shortcutcleaner import *
+import win32com.client
 
 def test_is_file_shortcut():
     assert is_file_shortcut("testfile.lnk") == True
@@ -26,3 +28,37 @@ def test_is_valid_url():
     assert is_valid_url('/data/Python.html') == False
     assert is_valid_url(532) == False
     assert is_valid_url(u'dkakasdkjdjakdjadjfalskdjfalk') == False
+
+def test_is_broken_shortcut( tmp_path ):
+    target_path = tmp_path / "target_file"
+    target_path.touch()
+    working_path = tmp_path / "working_shortcut.lnk"
+    broken_path1 = tmp_path / "broken_shortcut1.lnk"
+    broken_path2 = tmp_path / "broken_shortcut2.lnk"
+    broken_path3 = tmp_path / "broken_shortcut3.url"
+    broken_path4 = tmp_path / "broken_shortcut4.url"
+
+    shell = win32com.client.Dispatch("WScript.Shell")
+    working_shortcut = shell.CreateShortCut( str( working_path ) )
+    working_shortcut.Targetpath = str( target_path )
+    working_shortcut.save()
+
+    broken_shortcut1 = shell.CreateShortCut( str( broken_path1 ) )
+    broken_shortcut1.save()
+
+    broken_shortcut2 = shell.CreateShortCut( str( broken_path2 ) )
+    working_shortcut.Targetpath = str( tmp_path / "not_a_file" )
+    broken_shortcut2.save()
+
+    broken_shortcut3 = shell.CreateShortCut( str( broken_path4 ) )
+    broken_shortcut3.save()
+
+    broken_shortcut4 = shell.CreateShortCut( str( broken_path4 ) )
+    working_shortcut.Targetpath = "not_a_valid_url"
+    broken_shortcut4.save()
+
+    assert is_broken_shortcut( str( working_path ) ) == False
+    assert is_broken_shortcut( str( broken_path1 ) ) == True
+    assert is_broken_shortcut( str( broken_path2 ) ) == True
+    assert is_broken_shortcut( str( broken_path3 ) ) == True
+    assert is_broken_shortcut( str( broken_path4 ) ) == True
